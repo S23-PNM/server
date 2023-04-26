6624a61d7e483dd4fd471d7f7be7a9eced623418
Arif Ali <51419655+arifali123@users.noreply.github.com>
2023-04-25 22:14:03 -0700

    finish ingest route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 async function main() {
   const app = express();
+  const prisma = new PrismaClient();
   app.get("/", (_, res) => {
     return res.json({ hello: "hi" });
+  });
+  app.post("/api/ingest", async (req, res) => {
+    const { location, time, direction } = req.body;
+    await prisma.event.create({
+      data: {
+        location: location,
+        time: new Date(Math.round(time) * 1000),
+        enter: direction === "right",
+      },
+    });
+    res.status(204).send("");
   });
   const port = 3000 || process.env.PORT;
   app.listen(port, async () => {
