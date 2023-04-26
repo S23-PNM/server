a7d1ff1646ed31fb5e4ca503b268268ea73b98f3
Nyun Ei Hlaing <nyuneihlaing1@gmail.com>
2023-04-25 22:28:15 -0700

    test hook

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import { PrismaClient } from "@prisma/client";
-import express from "express";
+import express, { json } from "express";
 async function main() {
   const app = express();
+  app.use(json())
   const prisma = new PrismaClient();
   app.get("/", (_, res) => {
     return res.json({ hello: "hi" });
   });
   app.post("/api/ingest", async (req, res) => {
+    console.log(req.body)
     const { location, time, direction } = req.body;
     await prisma.event.create({
       data: {
         location: location,
-        time: new Date(Math.round(time) * 1000),
+        time: new Date(Math.round(time)),
         enter: direction === "right",
       },
     });
