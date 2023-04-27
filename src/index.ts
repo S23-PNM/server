e903aec4955d1c099e260cb3d9a818e08136c52f
Siddharth <hi@sid.fyi>
2023-04-26 18:45:26 -0700

    change port for testing

--- src/index.ts
+++ src/index.ts
@@ -74,7 +74,7 @@ async function main() {
 
     return res.json({ data: data });
   });
-  const port = 3000 || process.env.PORT;
+  const port = 8000 || process.env.PORT;
   app.listen(port, async () => {
     console.log(`Server is running on port ${port}`);
   });
